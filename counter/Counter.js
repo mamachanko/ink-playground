7226dd8f6fa56e52a9db5daf87f70ea33ff41d6a
mamachanko <sugardubz@gmail.com>
2019-03-19 09:12:52 +0100

    refactor

--- counter/Counter.js
+++ counter/Counter.js
@@ -5,8 +5,10 @@ const { render, Color } = require('ink');
 const Counter = () => {
     const [counter, setCounter] = React.useState(0);
 
+    const increment = () => setCounter(counter => counter + 1);
+
     React.useEffect(() => {
-        const interval = setInterval(() => setCounter(counter + 1), 100);
+        const interval = setInterval(increment, 100);
         return () => clearInterval(interval);
     });
 
