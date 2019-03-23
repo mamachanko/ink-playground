6f7faf585ac4d4ba39fb4a08401818e70418282c
mamachanko <sugardubz@gmail.com>
2019-03-23 12:56:25 +0100

    refactor Counter ts

--- counter-ts/Counter.test.tsx
+++ counter-ts/Counter.test.tsx
@@ -1,34 +1,39 @@
 import { cleanup, render } from 'ink-testing-library';
 import * as React from 'react';
+import * as TestRenderer from 'react-test-renderer';
 import stripAnsi from 'strip-ansi';
 import Counter from './Counter';
 
 describe('<Counter />', () => {
 
-    beforeEach(() => {
-        jest.useFakeTimers();
-    })
-
     afterEach(() => {
         cleanup();
         jest.clearAllTimers();
     });
+    
+    it('counts up', (done) => {
+        jest.useRealTimers();        
 
-    it('counts up', () => {
         const { lastFrame, stdin } = render(<Counter />);
-
+        
         expect(stripAnsi(lastFrame())).toContain('Counter: 0');
 
-        stdin.write(' ');
-        jest.advanceTimersByTime(500);
-        expect(stripAnsi(lastFrame())).toContain('Counter: 1');
+        setTimeout(() => {
+            jest.useFakeTimers();
 
-        stdin.write(' ');
-        jest.advanceTimersByTime(500);
-        expect(stripAnsi(lastFrame())).toContain('Counter: 2');
+            stdin.write(' ');
+            jest.advanceTimersByTime(500);
+            expect(stripAnsi(lastFrame())).toContain('Counter: 1');
 
-        stdin.write(' ');
-        jest.advanceTimersByTime(500);
-        expect(stripAnsi(lastFrame())).toContain('Counter: 3');
+            stdin.write(' ');
+            jest.advanceTimersByTime(500);
+            expect(stripAnsi(lastFrame())).toContain('Counter: 2');
+    
+            stdin.write(' ');
+            jest.advanceTimersByTime(500);
+            expect(stripAnsi(lastFrame())).toContain('Counter: 3');
+
+            done();
+        });
     });
 });
--- counter-ts/Counter.tsx
+++ counter-ts/Counter.tsx
@@ -1,52 +1,65 @@
 import { Box, Color, StdinContext } from 'ink';
 import Spinner from 'ink-spinner';
 import * as React from 'react';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, useCallback } from 'react';
 
 const CTRL_C = '\x03'
 const SPACE = ' ';
 
-const Counter = () => {
-    const {stdin, setRawMode} = useContext(StdinContext);
-    const [counter, setCounter] = useState(0);
-    const [incrementing, setIncrementing] = useState(false);
-
-    const increment = () => setCounter(counter => counter + 1);
-
-    const slowIncrement = () => {
-        if (incrementing) return;
-        setIncrementing(true);
-        setTimeout(() => {
-            increment();
-            setIncrementing(false);
-        }, 500);
-    }
-
-    const quit = () => {
-        console.log('ok. bye bye.');
-        process.exit(0);
-    }
-    
-    const handleInput = data => {
-        switch(data.toString()) {
-            case(CTRL_C): quit();
-            case(SPACE): slowIncrement();
-        }
-    };
-
-    setRawMode(true);
-    stdin.on('data', handleInput);
+const useStdin = handleInput => {
+    const { stdin, setRawMode } = useContext(StdinContext);
 
     useEffect(() => {
+        setRawMode(true);
+        stdin.on('data', handleInput);
+
         return () => {
             stdin.removeListener('data', handleInput);
             setRawMode(false);
         }
     });
+};
+
+const quit = () => {
+    console.log('ok. bye bye.');
+    process.exit(0);
+}
+
+const useSlowCounter = () => {
+    const [counter, setCounter] = useState(0);
+    const [isIncrementing, setIncrementing] = useState(false);
+    
+    const increment = useCallback(() => {
+        if (isIncrementing) return;
+        setIncrementing(true);
+        setTimeout(() => {
+            setCounter(counter => counter + 1);
+            setIncrementing(false);
+        }, 500);
+    }, [isIncrementing]);
+
+    return {counter, isIncrementing, increment};
+};
+
+const Counter = () => {
+    const {
+        counter, 
+        isIncrementing, 
+        increment
+    } = useSlowCounter();
+
+    useStdin(input => {
+        switch (input) {
+            case (CTRL_C): quit();
+            case (SPACE): increment();
+        }
+    });
 
     return <Box>
         <Box width={2}>
-            {incrementing ? <Spinner type='dots' />: undefined}
+            {isIncrementing ?
+                <Spinner type='dots' /> :
+                undefined}
         </Box>
         <Box>
             Counter: <Color green>{counter}</Color>
